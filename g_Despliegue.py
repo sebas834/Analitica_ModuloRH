94c67860aace05dfaa277b7ead3d4b491a602ab6
bvsebas1215 <102978765+bvsebas1215@users.noreply.github.com>
2023-03-07 21:53:44 -0500

    Add files via upload

---
+++ g_Despliegue.py
@@ -0,0 +1,7 @@
+import joblib
+
+#Cargar el modelo
+knn2 = joblib.load("knn2.pkl")
+
+# Se carga el modelo y queda disponible para que en el futuro 
+# cuando se tengan más datos el área de recursos humano lo pueda implementar.
